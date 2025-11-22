44cbdf7a84c135b64c9cf162a6c8db0370c94232
gaboprz <gabo20021228@estudiantec.cr>
2025-11-21 21:49:27 -0600

    Cambios varios

--- Servidor/sequence_item.sv
+++ Servidor/sequence_item.sv
@@ -31,7 +31,7 @@ class mesh_pkt extends uvm_sequence_item;
   }*/
 
   constraint c_external_device_column {
-    (target_col == 3) && (target_row == 3);
+    (target_col == 0) && (target_row == 0);
   }
 
   // >>> Rango simple y seguro para la holgura entre envíos
