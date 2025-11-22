eb822067fab73ebbc259214b8ef348dcc387bdb0
gaboprz <gabo20021228@estudiantec.cr>
2025-11-22 16:50:18 -0600

    Cambios varios

--- Servidor_Version_Vieja/test.sv
+++ Servidor_Version_Vieja/test.sv
@@ -26,7 +26,7 @@ class test extends uvm_test;
     virtual function void build_phase(uvm_phase phase);
         super.build_phase(phase);
 
-        uvm_top.set_timeout(1000000, 0);
+        uvm_top.set_timeout(500000, 0);
 
         env = mesh_env::type_id::create("env", this);
         uvm_config_db#(int unsigned)::set(this, "env", "NUM_DEVS", `NUM_DEVS);
@@ -66,7 +66,7 @@ class test extends uvm_test;
         test_list.push_back(prueba);
         prueba.name = "Prueba 2 - Un paquete en agente 1";
         prueba.num_packets_per_agent = '{
-            0: 11,  1: 41,  2: 21,  3:31,  4: 2,  5: 5,  6: 4,  7: 3,
+            0: 11,  1: 21,  2: 21,  3:31,  4: 2,  5: 5,  6: 4,  7: 3,
             8: 5,  9: 5,  10: 2, 11: 10, 12: 12, 13: 21, 14: 2, 15: 1
         };
         test_list.push_back(prueba);
