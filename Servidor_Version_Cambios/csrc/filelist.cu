8df92ccf1b9871b082fd16569a9953c34bc818ee
gaboprz <gabo20021228@gmail.com>
2025-11-24 22:27:20 -0600

    Varios

--- Servidor_Version_Cambios/csrc/filelist.cu
+++ Servidor_Version_Cambios/csrc/filelist.cu
@@ -5,16 +5,16 @@ ifeq ($(LDVERSION), 0)
 endif
 
 ARCHIVE_OBJS=
-ARCHIVE_OBJS += _3273425_archive_1.so
-_3273425_archive_1.so : archive.1/_3273425_archive_1.a
+ARCHIVE_OBJS += _4054650_archive_1.so
+_4054650_archive_1.so : archive.2/_4054650_archive_1.a
 	@$(AR) -s $<
-	@$(PIC_LD) -shared  -Bsymbolic $(LD_NORELAX_FLAG)  -o .//../salida.daidir//_3273425_archive_1.so --whole-archive $< --no-whole-archive
+	@$(PIC_LD) -shared  -Bsymbolic $(LD_NORELAX_FLAG)  -o .//../salida.daidir//_4054650_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
-	@ln -sf .//../salida.daidir//_3273425_archive_1.so $@
+	@ln -sf .//../salida.daidir//_4054650_archive_1.so $@
 
 
 ARCHIVE_OBJS += _prev_archive_1.so
-_prev_archive_1.so : archive.1/_prev_archive_1.a
+_prev_archive_1.so : archive.2/_prev_archive_1.a
 	@$(AR) -s $<
 	@$(PIC_LD) -shared  -Bsymbolic $(LD_NORELAX_FLAG)  -o .//../salida.daidir//_prev_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
